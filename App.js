c74622b930f13c2828dc0e09da44df06502e5f24
poojasalar <poojasalar998@gmail.com>
2024-12-13 17:02:48 -0600

    remove POO name

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>POOO</Text>
 
       <Text style={styles.text}>Emmanuel Oyenuga!</Text>
 
