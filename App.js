bea774e60b6f8b85b4da013624e0a1e6979caa53
poojasalar <poojasalar998@gmail.com>
2024-12-13 16:57:24 -0600

    fixed my name

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.Title}>Pooja SureshKumar Salar</Text>
+      <Text style={styles.Title}>Pooja Suresh Kumar Salar</Text>
       <StatusBar style="auto" />
     </View>
   );
